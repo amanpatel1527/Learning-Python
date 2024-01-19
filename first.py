12cce41538e7af7f1019f06ff734254839aa4b34
Aman Patel <patelpatelaman00@gmail.com>
2024-01-19 18:08:18 +0530

    first commit

---
+++ first.py
@@ -0,0 +1,25 @@
+# print("Hello Duniya")
+
+# Variables in pythonn
+# name  = "Aman"
+# age = 21
+# name = "Monu"
+# print( name )
+
+# is_adult = True;
+# print(is_adult )
+
+
+# first_name = "Aman"
+# last_name = "Patel"
+# age = 21
+# is_adult = True
+
+# print(  first_name +" " + last_name)
+# print(age)
+# print(is_adult )
+
+name = input("Enter name");
+print(name)
+age = input("Enter age");
+print(age)
